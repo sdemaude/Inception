3e5a59af324f2212669ef3b0005af9b56198fd55
sdemaude <sdemaude@gmail.com>
2024-10-11 12:59:44 +0200

    add commentary to files

--- srcs/requirements/mariadb/scripts/setup.sh
+++ srcs/requirements/mariadb/scripts/setup.sh
@@ -1,20 +1,30 @@
-#!/bin/sh
+#!/bin/sh	#?? Specify the shell interpreter
 
+# Start MariaDB in the background without attaching to the terminal
 mariadbd-safe --nowatch
 
+# Loop until MariaDB is ready to accept connections
 while ! mariadb-check -A
 do
 	sleep 1
 done
 
+# Create the database if it doesn't exist
 mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+# Create a user with specified username and password
 mariadb -e "CREATE USER IF NOT EXISTS $DB_USER@'localhost' IDENTIFIED BY '$DB_PASS';"
+# Grant all privileges on the database to the user, allowing remote access
 mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASS';"
+# Reload previleges tables to apply changes
 mariadb -e "FLUSH PRIVILEGES;"
 
+# Set the root password for MariaDB
 mariadb-admin -u root password "$DB_PASS"
+# Shut down the mariaDB server
 mariadb-admin -u root -p"$DB_PASS" shutdown
 
+# Create a file to signal that the setup is complete
 touch /good
 
+# Start MariaDB in the foreground
 mariadbd-safe
--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,30 +1,36 @@
-#!/bin/sh
+#!/bin/sh	#?? Specify the shell interpreter
 
+# Define the directory where WordPress is installed
 wp_dir='/var/www/html'
 
+# Check if the WordPress configuration file does not exist
 if [ ! -e "${wp_dir}/wp-config.php" ]
 then
 
+	# Create the WordPress confuiguration file with the specified database settings
 	wp config create	--allow-root \
-				--dbname=$DB_NAME \
-				--dbuser=$DB_USER \
-				--dbpass=$DB_PASS \
-				--dbhost=mariadb:3306 \
-				--path=$wp_dir
+				--dbname=$DB_NAME \		# Database name
+				--dbuser=$DB_USER \		# Database user
+				--dbpass=$DB_PASS \		# Database password
+				--dbhost=mariadb:3306 \		# Database host
+				--path=$wp_dir			# WP install path
 
+	# Install WordPress core files
 	wp core install		--allow-root \
-				--url=$WP_URL \
-				--title=$WP_TITLE \
-				--admin_email=$WP_ADMIN_EMAIL \
-				--admin_user=$WP_ADMIN \
-				--admin_password=$WP_ADMIN_PASS \
-				--path=$wp_dir
+				--url=$WP_URL \				# Site URL
+				--title=$WP_TITLE \			# Site title
+				--admin_email=$WP_ADMIN_EMAIL \		# Admin email
+				--admin_user=$WP_ADMIN \		# Admin username
+				--admin_password=$WP_ADMIN_PASS \	# Admin password
+				--path=$wp_dir				# WP install path
 
-	wp user create		--allow-root \
-				$WP_USER $WP_USER_EMAIL \
-				--user_pass=$WP_USER_PASS \
-				--path=$wp_dir
+	# Create a new WordPress user
+	wp user create		--allow-root \			
+				$WP_USER $WP_USER_EMAIL \	# New user and email
+				--user_pass=$WP_USER_PASS \	# New user's password 
+				--path=$wp_dir			# WP installation path
 
 fi
 
+# Start the PHP FastCGI Process Manager in the foreground
 /usr/sbin/php-fpm82 -F
